dbd3c1b888d8e9d474e570295270a373b1bea957
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-03-14 13:17:34 +0530

    Sarthak | Solution to concurrent  multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,8 +9,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                respond_to(&mut stream);
+            Ok(stream) => {
+                respond_to(stream);
             }
             Err(err) => {
                 println!("error: {}", err);
@@ -18,19 +19,21 @@ fn main() {
     }
 }
 
-fn respond_to(stream: &mut TcpStream) {
-    loop {
-        let mut content = [0u8; 14];
-        let result = stream.read(&mut content);
-        match result {
-            Ok(_) => {
-                stream.write_all(pong_response().as_bytes()).unwrap();
-            }
-            Err(_err) => {
-                break;
+fn respond_to(mut stream: TcpStream) {
+    thread::spawn(move || {
+        loop {
+            let mut content = [0u8; 14];
+            let result = stream.read(&mut content);
+            match result {
+                Ok(_) => {
+                    stream.write_all(pong_response().as_bytes()).unwrap();
+                }
+                Err(_err) => {
+                    break;
+                }
             }
         }
-    }
+    });
 }
 
 fn pong_response() -> &'static str {
