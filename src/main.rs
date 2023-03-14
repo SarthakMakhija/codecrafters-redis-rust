4c39cb73c63b7d5bb6e146879acd5a8662f6c813
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-03-14 11:49:15 +0530

    Sarthak | Solution to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-use std::io::Write;
+use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted new connection");
-                stream.write(pong_response().as_bytes()).unwrap();
+                respond_to(&mut stream);
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(err) => {
+                println!("error: {}", err);
+            }
+        }
+    }
+}
+
+fn respond_to(stream: &mut TcpStream) {
+    loop {
+        let mut content = [0u8; 14];
+        let result = stream.read(&mut content);
+        match result {
+            Ok(_) => {
+                stream.write_all(pong_response().as_bytes()).unwrap();
+            }
+            Err(_err) => {
+                break;
             }
         }
     }
